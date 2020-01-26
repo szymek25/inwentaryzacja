787c2d2560d0bca233e2fa6a62069a10885ec2d6
Pawel Szymanki <pszymanski@si-ecommerce.pl>
2020-01-26 12:22:04 +0100

    Initial changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+	public Button testBtn;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+	public void OnClickTest(javafx.event.ActionEvent actionEvent) throws IOException, InterruptedException {
+
+		Runtime r = Runtime.getRuntime();
+		Process p = r.exec("open -a \"Microsoft Excel\" /Users/wladysylawziomek/Desktop/inw/Wagi\\ .xlsx");
+		p.waitFor();
+		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line = "";
+
+		while ((line = b.readLine()) != null) {
+			System.out.println(line);
+		}
+
+		b.close();
+	}
 }
